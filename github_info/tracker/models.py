1eb0f08d7c96130cc0c5801c3bd8938948217810
Sam Lacey <sc.lacey90@gmail.com>
2016-03-13 16:20:43 +0000

    C004: Add basic caching
    To stop hammering github api and make everything faster

--- github_info/tracker/models.py
+++ github_info/tracker/models.py
@@ -1,8 +1,10 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
+from django.core.cache import cache
 from django.db import models
 
+import sha
 import urllib
 import urlparse
 
@@ -13,6 +15,9 @@ class Repo(models.Model):
     repo_name = models.CharField(
         max_length=settings.DEFAULT_FORM_CHAR_LIMIT,
     )
+
+    def get_issue_cache_key(self, page):
+        return sha.new(self.repo_name + str(page)).hexdigest()
 
     def get_issues(self, page=0):
         url = urlparse.urlunparse((
@@ -29,6 +34,13 @@ class Repo(models.Model):
             ''
         ))
 
-        resp = requests.get(url)
+        # Check cache to see if we already have got this data locally
+        cache_key = self.get_issue_cache_key(page)
+        cached_resp = cache.get(cache_key)
+        if cached_resp is not None:
+            return cached_resp
 
-        return resp.json()
+        resp = requests.get(url).json()
+        cache.set(cache_key, resp, settings.CACHE_LIFE_SPAN)
+
+        return resp
